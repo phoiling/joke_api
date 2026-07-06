b0c7cced9c0f8ee1a50344f419dd8202e5a2cac3
Dale leyland <daleleyland@icloud.com>
2026-07-06 13:39:45 +1200

    addd env to gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router, response::IntoResponse, routing::{future::RouteFuture, get}, serve::Listener};
+use serde_json::json;
+
+#[tokio::main]
+async fn main(){
+
+    let app = Router::new().route("/api", get(hello_world));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("sever started");
+    axum::serve(listener, app).await.unwrap();
+ 
+}
+
+async fn hello_world() -> impl IntoResponse {
+    let json_responce = json!({
+        "poo_size": "medum",
+        "message": "your a poo",
+    });
+    Json(json_responce)
 }
